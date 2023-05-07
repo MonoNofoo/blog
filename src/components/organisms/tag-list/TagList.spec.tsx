7830668f2d495b7f60fd6b3b8a1d7017a6516292
mono nofoo <dhntyt@gmail.com>
2023-05-07 13:29:57 +0900

    TagListのテスト

--- src/components/organisms/tag-list/TagList.spec.tsx
+++ src/components/organisms/tag-list/TagList.spec.tsx
@@ -2,10 +2,38 @@ import { render, screen } from '@testing-library/react';
 import TagList from '@/components/organisms/tag-list/TagList';
 
 describe('TagList について', () => {
-  it('tagId に応じたリンクが表示される', () => {
-    render(<TagList tagIds={['TYPE_SCRIPT']} />);
-    const target = screen.getByRole('link', { name: 'TypeScript' });
+  describe('tagId の挙動について', () => {
+    const tagIds = [
+      {
+        id: 'TYPE_SCRIPT',
+        urlValue: 'type-script',
+        label: 'TypeScript',
+      },
+      {
+        id: 'NEXT_JS',
+        urlValue: 'next-js',
+        label: 'Next.js',
+      },
+      {
+        id: 'REACT',
+        urlValue: 'react',
+        label: 'React',
+      },
+      {
+        id: 'SEO',
+        urlValue: 'seo',
+        label: 'SEO',
+      },
+    ] as const;
 
-    expect(target).toHaveAttribute('href', '/tags/type-script');
+    it.each(tagIds)(
+      'tagId に $id が渡されたとき適切なリンクが生成される',
+      ({ id, urlValue, label }) => {
+        render(<TagList tagIds={[id]} />);
+        const target = screen.getByRole('link', { name: label });
+
+        expect(target).toHaveAttribute('href', `/tags/${urlValue}`);
+      },
+    );
   });
 });
