fe02a3265926f616f7a069bfbe23844d78354c8e
mono nofoo <dhntyt@gmail.com>
2023-05-07 13:12:22 +0900

    storybook 動いてなかったので動くように

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,49 +1,62 @@
 const path = require('path');
-const {
-  VanillaExtractPlugin
-} = require('@vanilla-extract/webpack-plugin');
+const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   typescript: {
-    reactDocgen: false
+    reactDocgen: false,
   },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   framework: {
     name: '@storybook/nextjs',
-    options: {}
+    options: {},
   },
   staticDirs: ['../public'],
   webpackFinal(baseConfig) {
     baseConfig.resolve.alias = {
       ...baseConfig.resolve.alias,
       '@': path.resolve(__dirname, '../src'),
-      '@public': path.resolve(__dirname, '../public')
+      '@public': path.resolve(__dirname, '../public'),
     };
-    const {
-      module = {},
-      plugins = {}
-    } = baseConfig;
-    const cssRule = module.rules.find(rule => rule?.test?.test('test.css'));
-    cssRule.test = /.*(?<!\.vanilla)\.css$/;
-    return {
-      ...baseConfig,
-      plugins: [...plugins, new VanillaExtractPlugin(), new MiniCssExtractPlugin()],
-      module: {
-        ...module,
-        rules: [...module.rules, {
-          test: /\.vanilla\.css$/i,
-          use: [MiniCssExtractPlugin.loader, {
-            loader: require.resolve('css-loader'),
-            options: {
-              url: false
-            }
-          }]
-        }]
+
+    // Add Vanilla-Extract and MiniCssExtract Plugins
+    baseConfig.plugins?.push(
+      new VanillaExtractPlugin(),
+      new MiniCssExtractPlugin(),
+    );
+
+    // Exclude vanilla extract's "*.vanilla.css" files from other "*.css" processing
+    baseConfig.module?.rules?.forEach((rule) => {
+      if (
+        typeof rule !== 'string' &&
+        rule.test instanceof RegExp &&
+        rule.test.test('test.css')
+      ) {
+        rule.exclude = /\.vanilla\.css$/i;
       }
-    };
+    });
+
+    baseConfig.module?.rules?.push({
+      test: /\.vanilla\.css$/i,
+      use: [
+        MiniCssExtractPlugin.loader,
+        {
+          loader: require.resolve('css-loader'),
+          options: {
+            // Required as image imports should be handled via JS/TS import statements
+            url: false,
+          },
+        },
+      ],
+    });
+
+    return baseConfig;
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
